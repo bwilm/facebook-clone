b590ccad15521c1ac13dc031313457ff78b0cb29
Brandon <brandonwilmott@gmail.com>
2020-09-12 16:32:34 -0400

    firebase setup, app setup

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+var firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyAmvPBYsAM_JvxeihxOqUOaXVNGlbz0Tlc",
+    authDomain: "facebook-clone-11576.firebaseapp.com",
+    databaseURL: "https://facebook-clone-11576.firebaseio.com",
+    projectId: "facebook-clone-11576",
+    storageBucket: "facebook-clone-11576.appspot.com",
+    messagingSenderId: "440261744928",
+    appId: "1:440261744928:web:f5306a6f8484d13270df45",
+    measurementId: "G-FEHQLZDHX9"
+  });
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export{auth, db, storage}
